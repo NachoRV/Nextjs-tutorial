8a8d2486e8c6ba3949728b8fb9e001faa2707b5b
irvb <irvb>
2021-01-25 22:34:18 +0100

    save

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -33,13 +33,13 @@ export default function Post({ postData }) {
       </article>
     </MainLayout>
     <style jsx>{`
-    pre {
-    white-space: pre-wrap;       /* Since CSS 2.1 */
-    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
-    white-space: -pre-wrap;      /* Opera 4-6 */
-    white-space: -o-pre-wrap;    /* Opera 7 */
-    word-wrap: break-word;       /* Internet Explorer 5.5+ */
-}
+      pre {
+        white-space: pre-wrap;       /* Since CSS 2.1 */
+        white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+        white-space: -pre-wrap;      /* Opera 4-6 */
+        white-space: -o-pre-wrap;    /* Opera 7 */
+        word-wrap: break-word;       /* Internet Explorer 5.5+ */
+      }
     `}
     </style>
     </>
